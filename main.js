a44627c31f10fd21fa045e0fa76ce5fd48c034fb
Beatriz Correia De Araujo <108531051+tryxzbia@users.noreply.github.com>
2024-05-27 14:35:34 -0300

    s

---
+++ main.js
@@ -0,0 +1,31 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+
+
