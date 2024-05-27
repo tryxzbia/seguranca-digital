01ad37ad9f2c9b43d9c47d809fa8fec6f32c58c5
Beatriz Correia De Araujo <108531051+tryxzbia@users.noreply.github.com>
2024-05-27 15:02:19 -0300

    s

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ geraSenha();
 campoSenha.value = letrasMaiusculas;
 
 function geraSenha(){
-    let senha = '' ;
+    let senha = '' ”;
     for (let i = 0; i < tamanhoSenha;i++){
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
